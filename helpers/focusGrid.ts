898c8f8650fd008bd82fe6dd16a9af6edba8eb14
Amir.s.rezvani <154739563+ChiefInformationSecurityOfficer@users.noreply.github.com>
2024-02-04 22:39:58 +0330

    Add files via upload

---
+++ helpers/focusGrid.ts
@@ -0,0 +1,9 @@
+export const focusGrid = () => {
+  // Set focus back to Grid
+  const grid = document.querySelector('.pixi_canvas') as HTMLCanvasElement;
+  if (grid) {
+    grid.focus();
+  } else {
+    console.warn('`focusGrid()` tried to set the focus back to the grid, but couldn’t find it.');
+  }
+};
